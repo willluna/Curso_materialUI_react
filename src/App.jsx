4106bc8d0e7cd0ef404d6a44987827c8c6d96f78
william.luna <william.luna@rappi.com>
2023-12-19 12:35:08 -0500

    Curso Material UI #02 Botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,65 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import Button from '@mui/material/Button'
+
+
 
 function App() {
   return (
     <div>
-        <Button className='btn btn-primary'>Este es un boton</Button>
+      <Button color="secondary">
+        Color
+      </Button>
+
+      <Button
+      color="primary"
+      variant="contained"
+      >
+        Outlined
+      </Button>
+
+      <p>Lorem</p>
+      
+      <Button
+      className='mt-2 mb-4'
+      color="secondary"
+      variant='contained'
+      disableElevation 
+      >
+        Color
+      </Button>
+    <br></br>
+    <Button
+      color="secondary"
+      variant='contained'
+      href='https://google.com'
+      disableElevation
+      >
+        Google
+      </Button>
+    
+      <Button
+      color="secondary"
+      variant='contained'
+      fullWidth
+      >
+        fullWidth
+      </Button>
+
+      <Button
+      color="secondary"
+      variant='contained'
+      size='small'
+      >
+        fullWidth
+      </Button>
+
+      <Button
+      color="secondary"
+      variant='contained'
+      size='large'
+      >
+        fullWidth
+      </Button>
     </div>
   );
 }
