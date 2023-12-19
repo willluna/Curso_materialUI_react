bccc48b6df615ddfbffcb6ccf0c39ce2d996e08a
william.luna <william.luna@rappi.com>
2023-12-19 10:36:11 -0500

    Curso Material UI #01 Instalación

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+        <Button className='btn btn-primary'>Este es un boton</Button>
+    </div>
+  );
+}
+
+export default App;
